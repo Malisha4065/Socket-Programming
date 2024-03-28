0388a3384f8e3ce994e2deff8dbec3149dcc7ab2
Malisha A.P.D <128911037+Malisha4065@users.noreply.github.com>
2024-03-28 14:43:20 +0530

    initial commit

---
+++ src/main/java/org/example/first/Client.java
@@ -0,0 +1,60 @@
+package org.example.first;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class Client {
+    // initialize socket and input output streams
+    private Socket socket = null;
+    private DataInputStream input = null;
+    private DataOutputStream out = null;
+
+    // constructor to put ip address and port
+    public Client(String address, int port) throws IOException {
+        // establish a connection
+        try {
+            socket  = new Socket(address, port);
+            System.out.println("Connected");
+
+            // takes input from terminal
+            input = new DataInputStream(System.in);
+
+            // sends output to the socket
+            out = new DataOutputStream(socket.getOutputStream());
+        } catch (IOException e) {
+            System.out.println(e);
+            throw new RuntimeException(e);
+        }
+
+        // string to read message from input
+        String line = "";
+
+        //keep read until 'Over' is input
+        while(!line.equals("Over")) {
+            try {
+                line = input.readLine();
+                out.writeUTF(line);
+            }
+            catch (IOException e) {
+                System.out.println(e);
+            }
+        }
+
+        // close the connection
+        try {
+            input.close();
+            out.close();
+            socket.close();
+        }
+        catch (IOException e) {
+            System.out.println(e);
+        }
+    }
+
+    public static void main(String args[]) throws IOException {
+        Client client = new Client("127.0.0.1", 5000);
+    }
+}
